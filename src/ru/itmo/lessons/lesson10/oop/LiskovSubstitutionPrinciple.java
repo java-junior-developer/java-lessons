c9a5f64378bfd2b9af9235d1e1f1ef4edb3072c3
dasha <byrtosha@yandex.ru>
2023-04-05 14:54:17 +0300

    Занятие 10: принципы ООП

---
+++ src/ru/itmo/lessons/lesson10/oop/LiskovSubstitutionPrinciple.java
@@ -0,0 +1,57 @@
+package ru.itmo.lessons.lesson10.oop;
+
+public class LiskovSubstitutionPrinciple {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("Том");
+
+        Dog dog = new Dog();
+        dog.setName("Граф");
+
+        Human human = new Human();
+        human.catchAnimal(cat);
+        human.catchAnimal(dog);
+    }
+}
+abstract class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    abstract String saySomething();
+}
+
+class Cat extends Animal {
+
+    @Override
+    String saySomething() {
+        return "Мяу";
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    String saySomething() {
+        return "Гав";
+    }
+}
+
+
+class Human {
+    private Animal[] animals = new Animal[10];
+    public void catchAnimal(Animal animal){
+        for (int i = 0; i < animals.length; i++) {
+            if (animals[i] == null) {
+                System.out.println(animal.saySomething());
+                animals[i] = animal;
+            };
+        }
+    }
+}
---
+++ src/ru/itmo/lessons/lesson10/oop/OpenClosedPrinciple.java
@@ -0,0 +1,52 @@
+package ru.itmo.lessons.lesson10.oop;
+
+import java.util.Scanner;
+
+public class OpenClosedPrinciple {
+    public static void main(String[] args) {
+
+        IPay payWay;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите способ оплаты: 'ya' или 'sb'");
+        String userAnswer = scanner.nextLine();
+        if ("ya".equalsIgnoreCase(userAnswer)) {
+            payWay = new YaPay();
+        } else if ("sb".equalsIgnoreCase(userAnswer)) {
+            payWay = new SbPay();
+        } else {
+            System.out.println("Платеж не может быть проведен");
+            return;
+        }
+
+        System.out.println("Введите сумму");
+        double money = scanner.nextDouble();
+        payWay.pay(money);
+    }
+}
+
+interface IPay {
+    boolean pay(double money);
+}
+
+class YaPay implements IPay{
+
+    @Override
+    public boolean pay(double money) {
+        System.out.println("Оплата ya pay: " + money);
+        return true;
+    }
+
+}
+
+class SbPay implements IPay {
+
+    @Override
+    public boolean pay(double money) {
+        System.out.println("Оплата sb pay: " + money);
+        return true;
+    }
+}
+
+// при необходимость работы с другими типами платежных шлюзов
+// правильно добавить новый функционал в программу, не меняя код уже существующих классов
