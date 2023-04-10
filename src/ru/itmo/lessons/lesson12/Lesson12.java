b7e925ffae41862aebd19be3af591c63f0c52d51
dasha <byrtosha@yandex.ru>
2023-04-10 19:38:46 +0300

    Занятие 12

--- src/ru/itmo/lessons/lesson12/Lesson12.java
+++ src/ru/itmo/lessons/lesson12/Lesson12.java
@@ -3,6 +3,8 @@ package ru.itmo.lessons.lesson12;
 import ru.itmo.lessons.lesson06.books.Book;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 
 public class Lesson12 {
@@ -22,34 +24,38 @@ public class Lesson12 {
         // Исключения времени выполнения
         int a = 1;
         int b = 0;
-        int res;
-        res = a / b;
+        int res = 0;
+        // res = a / b; // java.lang.ArithmeticException
         System.out.println(res);
 
         int[] ints = new int[3];
-        ints[30] = 100;
+        // ints[30] = 100; // java.lang.ArrayIndexOutOfBoundsException
         System.out.println(Arrays.toString(ints));
 
         String string = null;
-        if (string.equals("null")) System.out.println("Строка содержит null");
+        /*
+        if (string.equals("null")) { // java.lang.NullPointerException
+            System.out.println("Строка содержит null");
+        }
+        */
 
         Object object = "123";
         String stringObject = (String) object;
         System.out.println(stringObject.toUpperCase());
-        Book book = (Book) object;
-        book.setTitle("Исключения в Java");
+        // Book book = (Book) object; // java.lang.ClassCastException
+        // book.setTitle("Исключения в Java");
 
         // Исключения времени компиляции
 
-        // Files.delete(Path.of("file.txt")); // IOException
+        // Files.delete(Path.of("file.txt")); // throw new IOException()
 
         // ОБРАБОТКА ИСКЛЮЧЕНИЙ любого типа (unchecked / checked)
 
         int x = 60, y = 0;
         int result = 0;
         try { // в блок try помещается потенциально опасный код,
-            // который может привести к исключению
-            result = x / y;
+              // который может привести к исключению
+            result = x / y; // throw new java.lang.ArithmeticException()
         } catch (ArithmeticException exception){ // перехватывает исключение
             // указанного в нем типа (в данном случае ArithmeticException) и его наследников
             System.out.println(exception.getMessage());
@@ -65,6 +71,17 @@ public class Lesson12 {
             throw new RuntimeException(e.getMessage(), e);
         }
 
+        // TODO::
+        //  1. объявить класс CalculatorException - исключение времени компиляции,
+        //  * конструктор должен принимать на вход сообщение и передавать его в соответствующий конструктор родителя
+        //  * переопределить метод getMessage на своё усмотрение
+        //  2. Объявить класс с набором статических методов:
+        //  * plus - возвращает результат сложения положительных чисел,
+        //  если числа не положительные необходимо выбросить CalculatorException
+        //  * minus - возвращает результат вычитания положительных чисел,
+        //  если числа не положительные необходимо выбросить CalculatorException
+        //  * random - возвращает случайное число в диапазоне от min до max
+        //  3. Вызвать методы в точке входа
 
 
     }
