11832575945acf0a034a916fa9ce9da42373a6a4
dasha <byrtosha@yandex.ru>
2023-04-12 23:11:02 +0300

    Занятие 13: Generic: типизированные методы, типизированные свойства

--- src/ru/itmo/lessons/lesson13/bounded/Lesson13Bounded.java
+++ src/ru/itmo/lessons/lesson13/bounded/Lesson13Bounded.java
@@ -13,12 +13,14 @@ public class Lesson13Bounded {
 
         // 1. public class Service<T extends Vehicle & Repaintable> {}
         // 2. поэтому в <> можно указать тип, который является типом Vehicle и типом Repaintable, плюс любых его наследников
-        // в данном случае только Car отвечает данным требованиям: Car extends Vehicle implements Repaintable
+        // в данном случае только Car и MiniCar отвечают данным требованиям:
+        // Car extends Vehicle implements Repaintable {}
+        // MiniCar extends Car {}
         // 3. в качестве generic свойства можно передать тип указанный в <> и всех его наследников
 
-        Service<Car> service01 = new Service<>(car01);
-        Service<Car> service02 = new Service<>(mini01);
-        Service<MiniCar> service03 = new Service<>(mini01);
+        Service<Car> service01 = new Service<>(car01); // car01 или mini01
+        Service<Car> service02 = new Service<>(mini01); // car01 или mini01
+        Service<MiniCar> service03 = new Service<>(mini01); // только mini01
 
     }
 
