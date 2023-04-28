ef25dadbc51ac5a638edff41f49de108a6b688e6
dasha <byrtosha@yandex.ru>
2023-04-29 00:06:39 +0300

    Занятие 20: домашнее задание

---
+++ src/ru/itmo/lessons/lesson20/task03/Task03.java
@@ -0,0 +1,38 @@
+package ru.itmo.lessons.lesson20.task03;
+
+
+import java.util.*;
+
+
+public class Task03 {
+    public static void main(String[] args) {
+        // Написать реализацию методов task01 - task05
+    }
+
+    public static Double task05(ArrayList<Article> articles){
+        // Вернуть средний возраст авторов статей
+        return null;
+    }
+    public static Author task04(ArrayList<Article> articles, Article.Category category){
+        // Вернуть самого молодого автора статей категории == category
+        return null;
+    }
+    public static HashMap<String, ArrayList<Article>> task03(ArrayList<Article> articles){
+        // Вернуть map, где ключи - имя автора,
+        // значения - списки его статей
+
+        return null;
+    }
+    public static HashSet<String> task03(ArrayList<Article> articles, Article.Category category){
+        // Вернуть множество email авторов статей категории == category
+        return null;
+    }
+    public static Set<Article> task02(ArrayList<Article> articles, Article.Category category, int min, int max){
+        // Вернуть множество статей категории == category, возраст авторов которых попадает в диапазон от min до max
+        return null;
+    }
+    public static LinkedList<Article> task01(ArrayList<Article> articles, Article.Category category){
+        // Вернуть список статей категории == category, опубликованных сегодня
+        return null;
+    }
+}
