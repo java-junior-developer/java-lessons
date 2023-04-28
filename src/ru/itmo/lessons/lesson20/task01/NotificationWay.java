9a16a2bdf0b75df4cca69664841ace118c4febe7
dasha <byrtosha@yandex.ru>
2023-04-29 00:06:19 +0300

    Занятие 20

---
+++ src/ru/itmo/lessons/lesson20/task01/NotificationWay.java
@@ -0,0 +1,25 @@
+package ru.itmo.lessons.lesson20.task01;
+
+import java.util.function.Predicate;
+
+@FunctionalInterface
+public interface NotificationWay {
+    void send(User user);
+    default NotificationWay add(NotificationWay way){
+        return u -> {
+            send(u);
+            way.send(u);
+        };
+    }
+
+    default NotificationWay addIf(NotificationWay way, Predicate<User> predicate){
+        return u -> {
+            send(u);
+            if (predicate.test(u)) {
+                way.send(u);
+            }
+        };
+    }
+}
+
+
---
+++ src/ru/itmo/lessons/lesson20/task01/Notifier.java
@@ -0,0 +1,25 @@
+package ru.itmo.lessons.lesson20.task01;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Notifier {
+    private HashSet<User> users;
+    public void addUser(User user) {
+        // добавлять, если:
+        // пользователь не null (при null выбрасывать исключение) +
+        // у пользователя есть хотя бы один основной контакт для уведомлений +
+        // если пользователь согласен на дополнительные уведомления,
+        // у него должен быть хотя бы один дополнительный контакт для уведомлений
+        users.add(Objects.requireNonNull(user));
+    }
+
+    public void notifyUsers(NotificationWay way){
+        users.forEach(way::send);
+        /*for (User user : this.users) {
+            way.send(user);
+        }*/
+    }
+
+}
