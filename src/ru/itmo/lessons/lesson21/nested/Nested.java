38c8e6a107a272d256acdcf5c161783434d3251f
dasha <byrtosha@yandex.ru>
2023-05-03 18:15:13 +0300

    Занятие 21: локальные, анонимные, внутренние, вложенные классы

---
+++ src/ru/itmo/lessons/lesson21/nested/Nested.java
@@ -0,0 +1,17 @@
+package ru.itmo.lessons.lesson21.nested;
+
+public class Nested {
+    public static void main(String[] args) {
+
+        Exam exam01 = new Exam(Exam.Subject.MATHS);
+
+        Exam.Mark mark01 = new Exam.Mark(20);
+        mark01.setDescription("Ученик набрал 20 баллов, тк правильно ответил на 20 вопросов из 70.");
+
+        exam01.setMark(mark01);
+
+        System.out.println(exam01.getMark().getValue());
+        System.out.println(exam01.getMark().getDescription());
+
+    }
+}
