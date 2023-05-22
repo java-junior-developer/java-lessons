5cca8952d43dea5c038893e22155f8381cdfe48f
dasha <byrtosha@yandex.ru>
2023-05-22 19:29:42 +0300

    Занятие 25: Сериализация и десериализация + сокет соединение

--- src/ru/itmo/lessons/lesson25/ServerApp.java
+++ src/ru/itmo/lessons/lesson25/ServerApp.java
@@ -6,15 +6,21 @@ import ru.itmo.lessons.lesson25.common.ReadWrite;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ServerApp {
     private int port;
+    private Map<String, Task> tasksMap;
 
     // При создании сервера в конструктор передается номер порта (int),
     // по которому клиенты смогут подключаться к серверу.
     public ServerApp(int port) {
         this.port = port;
+        tasksMap = new HashMap<>();
     }
 
     public void run(){
@@ -22,10 +28,9 @@ public class ServerApp {
             System.out.println("Сервер запущен...");
 
             while (true) {
-                Socket socket = serverSocket.accept(); // 3.1. устанавливает соединение с клиентом
+                Socket socket = serverSocket.accept(); // устанавливает соединение с клиентом
                 try (ReadWrite readWrite = new ReadWrite(socket)){
-                    read(readWrite);
-                    write(readWrite);
+                    sendResponse(readWrite, read(readWrite));
                 } /*catch (ClassCastException e) {
                     System.out.println("Класс Message не найден");
                 }*/ catch (IOException e){
@@ -38,33 +43,78 @@ public class ServerApp {
         }
     }
 
-    private void write(ReadWrite readWrite) {
-        Message answer = new Message("server"); // 3.3. формирует ответное сообщение (с произвольным текстом)
+    private void sendResponse(ReadWrite readWrite, Message requestMessage) { // выполнение запрошенной задачи
+        Message responseMessage;
+        if (requestMessage == null || !tasksMap.containsKey(requestMessage.getText())) {
+            responseMessage = new Message("Задача не может быть выполнена");
+        } else {
+            responseMessage = tasksMap.get(requestMessage.getText()).execute(this);
+        }
+        write(readWrite, responseMessage);
+    }
+
+    private void write(ReadWrite readWrite, Message message) {
         try {
-            readWrite.writeMessage(answer); // 3.4. отправляет сообщение клиенту
+            readWrite.writeMessage(message); // отправка сообщения клиенту
         } catch (IOException e) {
             System.out.println("Ошибка отправки сообщения");
         }
     }
 
 
-    private void read(ReadWrite readWrite){
-        Message fromClient = null; // 3.2. получает от клиента сообщение
+    private Message read(ReadWrite readWrite){
+        Message fromClient = null;
         try {
-            fromClient = readWrite.readMessage();
-            String requestText = fromClient.getText();
-            // обработка известных серверу запросов
+            fromClient = readWrite.readMessage(); // чтение сообщения от клиента
         } catch (IOException e) {
             System.out.println("Ошибка во время чтения");
         }
-        System.out.println(fromClient.getText());
+        return fromClient;
+    }
+
+    public void addTask(Task task) {
+        tasksMap.put(task.getName(), task);
+    }
+
+    public Map<String, Task> getTasksMap(){
+        return Collections.unmodifiableMap(tasksMap);
     }
 
 
-
-
     public static void main(String[] args) {
-        new ServerApp(2222).run();
+        Task help = new Task("/help", "список доступных запросов и их описание", serverApp -> {
+            StringBuilder builder = new StringBuilder();
+            for (Task task: serverApp.getTasksMap().values()) {
+                builder.append(task.getName())
+                        .append(" - ")
+                        .append(task.getDescription())
+                        .append("\n");
+            }
+            return new Message(builder.toString());
+        });
+
+        Task requests = new Task("/requests", "количество успешно обработанных запросов", serverApp -> {
+            long numberOfRequests = serverApp.getTasksMap().values().stream()
+                    .mapToLong(Task::getExecuted).sum();
+            return new Message(String.valueOf(numberOfRequests));
+        });
+
+        Task popular = new Task("/popular", "название самого популярного запроса", serverApp -> {
+            String taskName = serverApp.getTasksMap().values().stream()
+                    .filter(task -> task.getExecuted() > 0)
+                    .max(Comparator.comparing(Task::getExecuted))
+                    .flatMap(task -> task.getName().describeConstable())
+                    .orElse("нет выполненных запросов");
+            return new Message(taskName);
+        });
+
+        ServerApp serverApp = new ServerApp(2222);
+
+        serverApp.addTask(help);
+        serverApp.addTask(requests);
+        serverApp.addTask(popular);
+
+        serverApp.run();
         /*
         ServerSocket serverSocket = null; // позволит установить соединение
         // с клиентскими программами
