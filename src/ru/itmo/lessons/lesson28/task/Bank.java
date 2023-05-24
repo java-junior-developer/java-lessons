0b9db87889e8ff425a3fe3dba4ee8866f0e7ece7
dasha <byrtosha@yandex.ru>
2023-05-24 22:14:40 +0300

    Занятие 28: Многопоточность: синхронизация - базовый функционал, методы класса Object

--- src/ru/itmo/lessons/lesson28/task/Bank.java
+++ src/ru/itmo/lessons/lesson28/task/Bank.java
@@ -3,7 +3,11 @@ package ru.itmo.lessons.lesson28.task;
 public class Bank {
 
     public void transferMoney(Account src, Account dst, int money){
-        Thread transaction = new Thread(new Transaction(src, dst, money));
-        transaction.start();
+                                                        // 78 90
+        Thread transaction01 = new Thread(new Transaction(src, dst, money));
+        transaction01.start();
+                                                         // 90 78
+        Thread transaction02 = new Thread(new Transaction(dst, src, money));
+        transaction02.start();
     }
 }
--- src/ru/itmo/lessons/lesson28/task/Transaction.java
+++ src/ru/itmo/lessons/lesson28/task/Transaction.java
@@ -14,5 +14,38 @@ public class Transaction implements Runnable{
     @Override
     public void run() {
         // TODO перевод денежных средств со счета src на счет dst в количестве money
+        // чтобы избежать взаимной блокировки потоков:
+        // 1. либо вложенная синхронизация с условием: сначала всегда на большем (по ID) объекте
+        if (src.getId() > dst.getId()) {
+            synchronized (src) {
+                synchronized (dst) {
+                    if (src.getBalance() >= this.money) {
+                        src.setBalance(src.getBalance() - this.money);
+                        dst.setBalance(dst.getBalance() + this.money);
+                    }
+                }
+            }
+        } else {
+            synchronized (dst) {
+                synchronized (src) {
+                    if (src.getBalance() >= this.money) {
+                        src.setBalance(src.getBalance() - this.money);
+                        dst.setBalance(dst.getBalance() + this.money);
+                    }
+                }
+            }
+        }
+        // 2. либо отдельные synchronized блоки, если позволяет логика программы
+        // (ожидание dst объекта приемлемо)
+        /*
+        synchronized (src) {
+            if (src.getBalance() >= this.money) {
+               src.setBalance(src.getBalance() - this.money);
+            }
+        }
+        synchronized (dst) {
+            dst.setBalance(dst.getBalance() + this.money);
+        }
+        }*/
     }
 }
